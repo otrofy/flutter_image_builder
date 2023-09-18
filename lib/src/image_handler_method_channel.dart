fc86fe6a03394f9bd084c8f48aeb242f3ebfb7a0
miguelp211 <53634991+miguelp211@users.noreply.github.com>
2023-09-18 12:00:29 -0500

    comment on classes

--- lib/src/image_handler_method_channel.dart
+++ lib/src/image_handler_method_channel.dart
@@ -3,15 +3,23 @@ import 'package:flutter/services.dart';
 
 import 'image_handler_platform_interface.dart';
 
-/// An implementation of [ImageHandlerPlatform] that uses method channels.
+/// /// An implementation of [ImageHandlerPlatform] that interacts with the native platform
+/// through method channels to get platform-specific details.
+
 class MethodChannelImageHandler extends ImageHandlerPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('image_handler');
 
+  /// Retrieves the platform version from the native side.
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
-    return version;
+    try {
+      return await methodChannel.invokeMethod<String>('getPlatformVersion');
+    } catch (error) {
+      // Handle or log the error as per the requirement.
+      debugPrint("Failed to get platform version: $error");
+      return null;
+    }
   }
 }
--- lib/src/image_handler_platform_interface.dart
+++ lib/src/image_handler_platform_interface.dart
@@ -2,17 +2,20 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import 'image_handler_method_channel.dart';
 
+/// Represents a platform interface for image handling.
+/// All platform-specific implementations should extend this class.
 abstract class ImageHandlerPlatform extends PlatformInterface {
   /// Constructs a ImageHandlerPlatform.
   ImageHandlerPlatform() : super(token: _token);
 
   static final Object _token = Object();
+  // Holds the instance of the platform-specific implementation.
 
   static ImageHandlerPlatform _instance = MethodChannelImageHandler();
 
   /// The default instance of [ImageHandlerPlatform] to use.
-  ///
   /// Defaults to [MethodChannelImageHandler].
+
   static ImageHandlerPlatform get instance => _instance;
 
   /// Platform-specific implementations should set this with their own
