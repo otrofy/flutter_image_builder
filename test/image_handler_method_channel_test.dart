ac9ac319250f277ba85aaaec15aeca0aad9579bc
miguelp211 <53634991+miguelp211@users.noreply.github.com>
2023-09-19 09:23:29 -0500

    Imports of library

--- test/image_handler_method_channel_test.dart
+++ test/image_handler_method_channel_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:image_handler/image_handler_method_channel.dart';
+import 'package:image_handler/src/image_handler_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +9,8 @@ void main() {
   const MethodChannel channel = MethodChannel('image_handler');
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
       channel,
       (MethodCall methodCall) async {
         return '42';
@@ -18,7 +19,8 @@ void main() {
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
--- test/image_handler_test.dart
+++ test/image_handler_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:image_handler/image_handler_platform_interface.dart';
-import 'package:image_handler/image_handler_method_channel.dart';
 import 'package:image_handler/src/image_handler.dart';
+import 'package:image_handler/src/image_handler_method_channel.dart';
+import 'package:image_handler/src/image_handler_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockImageHandlerPlatform
